e390de077ff50e893395141329fd724d16d0399f
Ashok <ashokvasarla@gmail.com>
2016-06-16 17:54:12 +0530

    Today push 16 Jun 2016

---
+++ basic-tutorial1.c
@@ -0,0 +1,34 @@
+#include <gst/gst.h>
+
+int main(int argc, char *argv[] )
+{
+	GstElement *pipeline;
+	GstBus * bus;
+	GstMessage * message;
+	/* Intialize gstreamer */
+
+	gst_init( &argc, &argv);
+	
+	pipeline = gst_parse_launch("playbin uri=file://sintel_trailer-480p.webm",NULL);
+	
+	/* start playing */
+	gst_element_set_state(pipeline,GST_STATE_PLAYING);
+	
+	/* wait until error or EOF */
+	
+	bus = gst_element_get_bus(pipeline);
+	message = gst_bus_timed_pop_filtered ( bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+	
+	if( message != NULL)
+	{
+		gst_message_unref(message);
+		
+	}
+	gst_object_unref(bus);
+	gst_element_set_state(pipeline,GST_STATE_NULL);
+	gst_object_unref(pipeline);
+	return 0;
+	
+	
+
+}
